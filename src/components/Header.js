a258a0c8d1adbb3a728d7e3ea219ba094d1dd953
Ernesto B <ejbcode@gmail.com>
2020-08-18 07:50:02 -0400

    First commit, add router-dom, and css

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content between">
+      <div className="container">
+        <h1>Title</h1>
+      </div>
+      <a
+        className="btn btn-danger nuevo-post d-block d-md-inline-block "
+        href="/products/new"
+      >
+        Add Product &#43;
+      </a>
+    </nav>
+  );
+};
+
+export default Header;
